093943973d58a7b90436d5c4b6218c49026bee1e
immadzenkoders <immad.zenkoders@gmail.com>
2024-06-10 16:28:17 +0500

    comm

---
+++ src/app/contact/page.jsx
@@ -0,0 +1,35 @@
+import Image from "next/image";
+import styles from "./contact.module.css";
+
+
+export const metadata = {
+  title: "Contact Page",
+  description: "Contact description",
+};
+
+const ContactPage = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.imgContainer}>
+        <Image src="/contact.png" alt="" fill className={styles.img} />
+      </div>
+      <div className={styles.formContainer}>
+        <form action="" className={styles.form}>
+          <input type="text" placeholder="Name and Surname" />
+          <input type="text" placeholder="Email Addres" />
+          <input type="text" placeholder="Phne Number (Optional)" />
+          <textarea
+            name=""
+            id=""
+            cols="38"
+            rows="10"
+            placeholder="Message"
+          ></textarea>
+          <button>Send</button>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default ContactPage;
